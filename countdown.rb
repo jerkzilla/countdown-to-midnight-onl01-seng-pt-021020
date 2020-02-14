81c1769cf1c34267499a531883affaef81156c4c
John J Marshall Jr <johnjamesmarshalljr@gmail.com>
2020-02-14 13:22:29 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,8 +2,8 @@
 
 def countdown(num)
   while num > 10 
-    #puts num.to_s + ' SECOND(S)!'
-    puts "#{num} SECOND(S)!"
+    puts num.to_s + ' SECOND(S)!'
+    #puts "#{num} SECOND(S)!"
     num -= 1
   
   end
